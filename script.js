577c159e64ddb069d08bfc413e11cc9857d90a20
chandu916 <cchandhan021@gmail.com>
2024-04-22 11:00:51 +0530

    major changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Jquary
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 50){
@@ -5,12 +6,10 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");  
         }
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show")
-        }
+
     });
+
+    
     // slide up script
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop:0});
